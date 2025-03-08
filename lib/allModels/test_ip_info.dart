3939d33d10f2670357bf6f78db4f2bb25c69fbc6
Ngo-tech <companyngo81@gmail.com>
2025-03-08 02:06:03 -0700

    Update test_ip_info.dart

--- lib/allModels/test_ip_info.dart
+++ lib/allModels/test_ip_info.dart
@@ -1,2 +1,15 @@
 import 'package:test/test.dart';
 import 'package:your_package/ip_info.dart'; // Change this to the actual path of your IPInfo class
+
+void main() {
+  // Test the constructor with incorrect expectations
+  test('IPInfo constructor initializes incorrectly', () {
+    final ipInfo = IPInfo(
+      countryName: 'USA',
+      regionName: 'California',
+      cityName: 'Los Angeles',
+      zipCode: '90001',
+      timezone: 'PST',
+      internetServiceProvider: 'Comcast',
+      query: '192.168.0.1',
+    );
