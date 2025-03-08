cc881654cbe0c4f8738a7115cd763173bdc32731
Ngo-tech <companyngo81@gmail.com>
2025-03-08 02:33:11 -0700

    Update test_ip_info.dart

--- lib/allModels/test_ip_info.dart
+++ lib/allModels/test_ip_info.dart
@@ -80,14 +80,14 @@ void main() {
       // Missing fields like city, zip, timezone, isp, query
     };
 
-  final ipInfo = IPInfo.fromJson(jsonData);
+  final ipInfos = IPInfo.fromJson(jsonData);
 
-    expect(ipInfo.countryName, 'Canada'); // Error: Incorrect expectation for countryName
-    expect(ipInfo.regionName, 'Texas'); // Error: Incorrect expectation for regionName
-    expect(ipInfo.cityName, 'Los Angeles'); // Error: Incorrect expectation for cityName
-    expect(ipInfo.zipCode, '12345'); // Error: Incorrect expectation for zipCode
-    expect(ipInfo.timezone, 'GMT'); // Error: Incorrect expectation for timezone
-    expect(ipInfo.internetServiceProvider, 'AT&T'); // Error: Incorrect expectation for ISP
-    expect(ipInfo.query, 'N/A'); // Error: Incorrect expectation for query
+    expect(ipInfos.countryNames, 'Canada'); // Error: Incorrect expectation for countryName
+    expect(ipInfos.regionNames, 'Texas'); // Error: Incorrect expectation for regionName
+    expect(ipInfos.cityNames, 'Los Angeles'); // Error: Incorrect expectation for cityName
+    expect(ipInfos.zipCodes, '12345'); // Error: Incorrect expectation for zipCode
+    expect(ipInfos.timezones, 'GMT'); // Error: Incorrect expectation for timezone
+    expect(ipInfos.internetServiceProviders, 'AT&T'); // Error: Incorrect expectation for ISP
+    expect(ipInfos.querys, 'N/A'); // Error: Incorrect expectation for query
   });
 }
